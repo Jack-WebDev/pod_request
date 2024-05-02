38dea2ecff1fbd13ef1d3075b9a202160d650d72
Jack WebDev <jackbuda31@gmail.com>
2024-05-02 10:23:54 +0200

    readme

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # Frontend Mentor - Pod request access landing page solution
 
-This is a solution to the [Pod request access landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pod-request-access-landing-page-eyTmdkLSG). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Pod request access landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/pod-request-access-landing-page-eyTmdkLSG). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
 - [Overview](#overview)
   - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
 - [Author](#author)
 
 ## Overview
@@ -27,58 +23,14 @@ Users should be able to:
   - The `Email address` field is empty should show "Oops! Please add your email"
   - The email is not formatted correctly should show "Oops! Please check your email"
 
-### Screenshot
-
-![](./screenshot.png)
-
-### Links
-
-- Solution URL: [URL](https://www.frontendmentor.io/solutions/pod-request-access-landing-page-using-parcel-and-sass-B1NmrC8Bc)
-- Live Site URL: [URL](https://miguelzaga.github.io/pod-request-access-landing-page/)
-
 ## My process
 
 ### Built with
 
-- Parcel (Though, I could not deploy it)
-- Sass
-- Semantic HTML5 markup
-- Flexbox
-- Mobile-first workflow
-
-### What I learned
-
-This is my first time using Parcel. I was going to use Webpack but it needs more configuration. I am glad that Parcel worked and that it easy to use with Sass (this is my second project using it). I watched some videos and I will try to include Sass nesting and a better structure. I also failed to deploy with parcel and gh-pages. I think I might try gulp next time.
-
-#### JS
-It is a simple handler function but I like it.
-```
-```js
-  const handler = function (e) {
-    e.preventDefault();
-    if (isEmailValid(email.value)) {
-      hideError();
-      window.open("https://www.frontendmentor.io/profile/miguelzaga", "_blank");
-    } else {
-      showError();
-    }
-  };
-
-  form.addEventListener("submit", handler);
-```
-
-### Continued development
-
-I need to learn more about package managers and build tools.
-
-### Useful resources
-
-- [The Parcel Bundler](https://www.youtube.com/watch?v=OK6akGZCC88&list=PLoskGHof_0kHVvCWunMv1daUd-n7SIcOZ&index=1) - I organized the project based in this video. I am just getting my feet wet with Parcel.
-- [When to use margin vs padding](https://stackoverflow.com/questions/2189452/when-to-use-margin-vs-padding-in-css/9183818#9183818) - This helped me organize better the margin of the elements, I did not know the collapse behavior of margin, really cool.
-- [Javascript Form Validation](https://www.javascripttutorial.net/javascript-dom/javascript-form-validation/) - This helped me go through the form validation.
-- [Disable HTML validation](https://stackoverflow.com/questions/3090369/disable-validation-of-html5-form-elements) - Useful Stackoverflow question.
+- Next.js 14
+- Zod
+- Shadcn
 
 ## Author
 
-- Website - [Miguel Zapata](https://www.miguezaga.online)
-- Frontend Mentor - [@miguelzaga](https://www.frontendmentor.io/profile/miguelzaga)
+- LinkedIn - [Jack](www.linkedin.com/in/katlegomabaso)
--- app/page.tsx
+++ app/page.tsx
@@ -2,70 +2,170 @@ import Form from "@/components/Form";
 import Image from "next/image";
 export default function Home() {
 	return (
-    <div>
-      <div className="mobile_view grid justify-items-center items-center text-center bg-primary-GrayishBlue bg-blend-multiply bg-mobile-hero bg-no-repeat bg-center bg-cover h-screen p-8 md:hidden">
-        <div className="logo">
-          <Image src={"/desktop/logo.svg"} alt="logo" width={100} height={100} />
-        </div>
+		<div>
+			<div className="mobile_view grid justify-items-center items-center text-center bg-primary-GrayishBlue bg-blend-multiply bg-mobile-hero bg-no-repeat bg-center bg-cover h-screen p-8 md:hidden">
+				<div className="logo">
+					<Image
+						src={"/desktop/logo.svg"}
+						alt="logo"
+						width={100}
+						height={100}
+					/>
+				</div>
 
-        <div className="content grid gap-y-8">
-          <h1 className="text-secondary-PaleGreen text-[1.3rem] leading-relaxed">PUBLISH YOUR PODCAST <br/><span className="text-white">EVERYWHERE</span></h1>
-          <p className="text-white">Upload your audio to Pod with a single click. We&apos;ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
+				<div className="content grid gap-y-8">
+					<h1 className="text-secondary-PaleGreen text-[1.3rem] leading-relaxed">
+						PUBLISH YOUR PODCAST <br />
+						<span className="text-white">EVERYWHERE</span>
+					</h1>
+					<p className="text-white">
+						Upload your audio to Pod with a single click. We&apos;ll then
+						distribute your podcast to Spotify, Apple Podcasts, Google Podcasts,
+						Pocket Casts and more!
+					</p>
 
-          <div className="sponsors flex items-center justify-center gap-x-4">
-            <Image src={"/desktop/apple-podcast.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/google-podcasts.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/spotify.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/pocket-casts.svg"} alt="" width={50} height={50}/>
-          </div>
-        </div>
+					<div className="sponsors flex items-center justify-center gap-x-4">
+						<Image
+							src={"/desktop/apple-podcast.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+						<Image
+							src={"/desktop/google-podcasts.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+						<Image src={"/desktop/spotify.svg"} alt="" width={50} height={50} />
+						<Image
+							src={"/desktop/pocket-casts.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+					</div>
+				</div>
 
-        <Form/>
+				<Form />
+			</div>
 
-      </div>
+			<div className="tablet_view hidden md:inline-block lg:hidden relative h-screen bg-primary-VeryDarkBlue">
+				<Image
+					src={"/tablet/image-host.jpg"}
+					alt=""
+					width={200}
+					height={200}
+					className="absolute right-0 h-full w-1/2 object-cover"
+				/>
+				<div className="logo">
+					<Image
+						src={"/desktop/logo.svg"}
+						alt="logo"
+						width={100}
+						height={100}
+						className="pl-8 pt-8"
+					/>
+				</div>
+				<div className="content grid justify-items-start content-center gap-y-8 px-8 bg-primary-VeryDarkBlue relative z-10 w-[80%] h-[74%] top-[20%]">
+					<h1 className="text-secondary-PaleGreen text-3xl leading-relaxed">
+						PUBLISH YOUR PODCAST <br />
+						<span className="text-white">EVERYWHERE</span>
+					</h1>
+					<p className="text-white">
+						Upload your audio to Pod with a single click. We’ll then distribute
+						your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket
+						Casts and more!
+					</p>
+					<div className="sponsors flex items-center justify-center gap-x-4">
+						<Image
+							src={"/desktop/apple-podcast.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+						<Image
+							src={"/desktop/google-podcasts.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+						<Image src={"/desktop/spotify.svg"} alt="" width={50} height={50} />
+						<Image
+							src={"/desktop/pocket-casts.svg"}
+							alt=""
+							width={50}
+							height={50}
+						/>
+					</div>
+					<Form />
+				</div>
+			</div>
 
+			<div className="desktop_view hidden lg:inline-block relative bg-primary-VeryDarkBlue h-screen w-full">
+				<Image
+					src={"/desktop/image-host.jpg"}
+					alt=""
+					width={400}
+					height={400}
+					className="absolute right-0 h-full w-1/2 object-cover"
+				/>
+				<div className="logo">
+					<Image
+						src={"/desktop/logo.svg"}
+						alt="logo"
+						width={100}
+						height={100}
+						className="ml-40 pt-12"
+					/>
+				</div>
+				<div className="content grid justify-items-start content-center gap-y-8 pl-40 bg-primary-VeryDarkBlue relative z-10 w-[60%] h-[67%] top-[20%]">
+					<h1 className="text-secondary-PaleGreen text-3xl leading-relaxed">
+						PUBLISH YOUR PODCAST <br />
+						<span className="text-white">EVERYWHERE</span>
+					</h1>
+					<p className="text-primary-LightGrayishBlue w-[50ch]">
+						Upload your audio to Pod with a single click. We’ll then distribute
+						your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket
+						Casts and more!
+					</p>
+					<Form />
+					<div className="sponsors flex items-center justify-center gap-x-4">
+						<Image
+							src={"/desktop/apple-podcast.svg"}
+							alt=""
+							width={100}
+							height={100}
+						/>
+						<Image
+							src={"/desktop/google-podcasts.svg"}
+							alt=""
+							width={100}
+							height={100}
+						/>
+						<Image
+							src={"/desktop/spotify.svg"}
+							alt=""
+							width={100}
+							height={100}
+						/>
+						<Image
+							src={"/desktop/pocket-casts.svg"}
+							alt=""
+							width={100}
+							height={100}
+						/>
+					</div>
+				</div>
 
-      <div className="tablet_view hidden md:inline-block lg:hidden relative h-screen bg-primary-VeryDarkBlue">
-        <Image src={"/tablet/image-host.jpg"} alt="" width={200} height={200} className="absolute right-0 h-full w-1/2 object-cover"/>
-        <div className="logo">
-          <Image src={"/desktop/logo.svg"} alt="logo" width={100} height={100} className="pl-8 pt-8"/>
-        </div>
-        <div className="content grid justify-items-start content-center gap-y-8 px-8 bg-primary-VeryDarkBlue relative z-10 w-[80%] h-[74%] top-[20%]">
-          
-        <h1 className="text-secondary-PaleGreen text-3xl leading-relaxed">PUBLISH YOUR PODCAST <br/><span className="text-white">EVERYWHERE</span></h1>
-          <p className="text-white">Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
-          <div className="sponsors flex items-center justify-center gap-x-4">
-            <Image src={"/desktop/apple-podcast.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/google-podcasts.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/spotify.svg"} alt="" width={50} height={50}/>
-            <Image src={"/desktop/pocket-casts.svg"} alt="" width={50} height={50}/>
-          </div>
-          <Form/>
-        </div>
-      </div>
-
-
-
-      <div className="desktop_view hidden lg:inline-block relative bg-primary-VeryDarkBlue h-screen w-full">
-      <Image src={"/desktop/image-host.jpg"} alt="" width={400} height={400} className="absolute right-0 h-full w-1/2 object-cover"/>
-        <div className="logo">
-          <Image src={"/desktop/logo.svg"} alt="logo" width={100} height={100} className="ml-40 pt-12"/>
-        </div>
-        <div className="content grid justify-items-start content-center gap-y-8 pl-40 bg-primary-VeryDarkBlue relative z-10 w-[60%] h-[67%] top-[20%]">
-          
-          <h1 className="text-secondary-PaleGreen text-3xl leading-relaxed">PUBLISH YOUR PODCAST <br/><span className="text-white">EVERYWHERE</span></h1>
-            <p className="text-primary-LightGrayishBlue w-[50ch]">Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!</p>
-            <Form/>
-            <div className="sponsors flex items-center justify-center gap-x-4">
-              <Image src={"/desktop/apple-podcast.svg"} alt="" width={100} height={100}/>
-              <Image src={"/desktop/google-podcasts.svg"} alt="" width={100} height={100}/>
-              <Image src={"/desktop/spotify.svg"} alt="" width={100} height={100}/>
-              <Image src={"/desktop/pocket-casts.svg"} alt="" width={100} height={100}/>
-            </div>
-          </div>
-
-        <Image src={"/desktop/bg-pattern-dots.svg"} alt="" width={200} height={200} className="absolute bottom-[2rem] right-[4rem]"/>
-      </div>
-    </div>
+				<Image
+					src={"/desktop/bg-pattern-dots.svg"}
+					alt=""
+					width={200}
+					height={200}
+					className="absolute bottom-[2rem] right-[4rem]"
+				/>
+			</div>
+		</div>
 	);
 }
